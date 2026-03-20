bf0449e2c3dd8e8b86e70fd317a799af450d6f0c
Mark Paluch <mark.paluch@broadcom.com>
2026-03-20 16:58:41 +0100

    Polishing.

--- mongodb/example/src/test/java/example/springdata/mongodb/bulk/BulkWriteIntegrationTest.java
+++ mongodb/example/src/test/java/example/springdata/mongodb/bulk/BulkWriteIntegrationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 the original author or authors.
+ * Copyright 2025-present the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,12 +15,14 @@
  */
 package example.springdata.mongodb.bulk;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.data.mongodb.core.query.Criteria.*;
 
 import example.springdata.mongodb.util.MongoContainers;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.mongodb.test.autoconfigure.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoOperations;
@@ -30,13 +32,14 @@ import org.springframework.data.mongodb.core.bulk.BulkWriteResult;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.mongodb.MongoDBContainer;
 
 /**
- * Integration test demonstrating {@link MongoOperations#bulkWrite(Bulk, BulkWriteOptions)} with a combination of
- * insert and update operations in a single request.
+ * Integration test demonstrating {@link MongoOperations#bulkWrite(Bulk, BulkWriteOptions)} with a combination of insert
+ * and update operations in a single request.
  *
  * @author Christoph Strobl
  */
@@ -44,16 +47,14 @@ import org.testcontainers.mongodb.MongoDBContainer;
 @DataMongoTest
 class BulkWriteIntegrationTest {
 
-	@Container
-	private static MongoDBContainer mongoDBContainer = MongoContainers.getDefaultContainer();
+	@Container private static MongoDBContainer mongoDBContainer = MongoContainers.getDefaultContainer();
 
 	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry registry) {
 		registry.add("spring.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
 	}
 
-	@Autowired
-	MongoOperations operations;
+	@Autowired MongoOperations operations;
 
 	@BeforeEach
 	void setUp() {
@@ -63,12 +64,12 @@ class BulkWriteIntegrationTest {
 	@Test
 	void bulkWriteInsertAndUpdateInOrder() {
 
-		Bulk bulk = Bulk.create(builder -> builder.inCollection(Jedi.class, spec -> spec
-				.insert(new Jedi("Luke", "Skywalker"))
-				.insert(new Jedi("Leia", "Princess"))
-				.updateOne(where("firstname").is("Leia"), new Update().set("lastname", "Organa")))
-			.inCollection(Sith.class, spec -> spec.upsert(where("name").is("Darth Sidious"), Update.update("realName", "Palpatine")))
-		);
+		Bulk bulk = Bulk.create(builder -> builder
+				.inCollection(Jedi.class,
+						spec -> spec.insert(new Jedi("Luke", "Skywalker")).insert(new Jedi("Leia", "Princess"))
+								.updateOne(where("firstname").is("Leia"), new Update().set("lastname", "Organa")))
+				.inCollection(Sith.class,
+						spec -> spec.upsert(where("name").is("Darth Sidious"), Update.update("realName", "Palpatine"))));
 
 		BulkWriteResult result = operations.bulkWrite(bulk, BulkWriteOptions.ordered());
 
@@ -77,12 +78,9 @@ class BulkWriteIntegrationTest {
 		assertThat(result.modifiedCount()).isOne(); // leia
 		assertThat(result.upsertCount()).isOne(); // darth sidious
 
-		assertThat(operations.findAll(Jedi.class))
-			.extracting(Jedi::fullName)
-			.containsExactlyInAnyOrder("Luke Skywalker", "Leia Organa");
+		assertThat(operations.findAll(Jedi.class)).extracting(Jedi::fullName).containsExactlyInAnyOrder("Luke Skywalker",
+				"Leia Organa");
 
-		assertThat(operations.findAll(Sith.class))
-			.extracting(Sith::name)
-			.containsExactlyInAnyOrder("Darth Sidious");
+		assertThat(operations.findAll(Sith.class)).extracting(Sith::name).containsExactlyInAnyOrder("Darth Sidious");
 	}
 }
